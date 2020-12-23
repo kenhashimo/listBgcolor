d1090f9e1129e596cacae80bd75cb44beb17a629
Ken Hashimoto <kenhashimo@gmail.com>
2020-12-23 13:47:41 +0900

    改行子の変換を修正

--- listBgcolorFormat.pm
+++ listBgcolorFormat.pm
@@ -7,7 +7,9 @@ sub format {
   my ($value, $color) = ($_[0], $_[1]);
   $value =~ s/\\/\\\\/g;
   $value =~ s/"/\\"/g;
-  $value =~ s/\n/\\n/g;
+  $value =~ s/\x0d\x0a/\\n/g;
+  $value =~ s/\x0d/\\n/g;
+  $value =~ s/\x0a/\\n/g;
   return sprintf("  bgColor[\"%s\"] = '#%s';\n", $value, $color);
 }
 
